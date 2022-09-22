2ff919da37a28acc98a7ecebc867280e58f94138
arfan36 <arfanur36@gmail.com>
2022-09-23 00:06:17 +0600

    46_5-1 Recap Create react app,Component, styles, props

--- src/App.js
+++ src/App.js
@@ -4,20 +4,24 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <District name="NoaKhali" special="bivag"></District>
+      <District name="Bramonbaria" special="joda akbar"></District>
+      <District name="Sumilla" special="Moyna and modi"></District>
+    </div>
+  );
+}
+
+const districtStyle = {
+  backgroundColor: 'gray',
+  margin: '20px',
+  borderRadius: '20px'
+};
+
+function District(props) {
+  return (
+    <div style={districtStyle}>
+      <h2>Name: {props.name}</h2>
+      <p>Specialty: {props.special}</p>
     </div>
   );
 }
